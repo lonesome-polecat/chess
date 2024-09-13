678c6797c5258bd3bd0593f9d327421ea092bd76
Helaman Bennion <helama22@byu.edu>
2024-09-13 07:47:28 -0600

    passoff all rook tests

--- shared/src/main/java/chess/ChessRules.java
+++ shared/src/main/java/chess/ChessRules.java
@@ -22,17 +22,91 @@ public class ChessRules {
         // now check for more normal character movement
         // check front/back movement
         if (rules.charAt(0) == '1') {
-            // put rules here
+            getForwardBackMoves(validMoves, board, startPos);
         }
         // Check left/right movement
         if (rules.charAt(1) == '1') {
-            // put rules here
+            getRightLeftMoves(validMoves, board, startPos);
         }
         if (rules.charAt(2) == '1') {
             getDiagonalMoves(validMoves, board, startPos);
         }
 
         return validMoves;
+    }
+
+    public static void getForwardBackMoves(ArrayList<ChessMove> validMoves, ChessBoard board, ChessPosition startPos) {
+
+        int row = startPos.getRow();
+        int col = startPos.getColumn();
+        // first check down
+        while (row > 1) {
+            row -= 1;
+            var possiblePos = new ChessPosition(row, col);
+            ChessPiece pieceOnSquare = board.getPiece(possiblePos);
+            if (pieceOnSquare == null) {
+                validMoves.add(new ChessMove(startPos, possiblePos, null));
+            } else {
+                // Only add possible move if the piece on square is an enemy piece
+                if (pieceOnSquare.getTeamColor() != board.getPiece(startPos).getTeamColor()) {
+                    validMoves.add(new ChessMove(startPos, possiblePos, null));
+                }
+                break;
+            }
+        }
+        row = startPos.getRow();
+        // then check up
+        while (row < 8) {
+            row += 1;
+            var possiblePos = new ChessPosition(row, col);
+            ChessPiece pieceOnSquare = board.getPiece(possiblePos);
+            if (pieceOnSquare == null) {
+                validMoves.add(new ChessMove(startPos, possiblePos, null));
+            } else {
+                // Only add possible move if the piece on square is an enemy piece
+                if (pieceOnSquare.getTeamColor() != board.getPiece(startPos).getTeamColor()) {
+                    validMoves.add(new ChessMove(startPos, possiblePos, null));
+                }
+                break;
+            }
+        }
+    }
+
+    public static void getRightLeftMoves(ArrayList<ChessMove> validMoves, ChessBoard board, ChessPosition startPos) {
+
+        int row = startPos.getRow();
+        int col = startPos.getColumn();
+        // first check left
+        while (col > 1) {
+            col -= 1;
+            var possiblePos = new ChessPosition(row, col);
+            ChessPiece pieceOnSquare = board.getPiece(possiblePos);
+            if (pieceOnSquare == null) {
+                validMoves.add(new ChessMove(startPos, possiblePos, null));
+            } else {
+                // Only add possible move if the piece on square is an enemy piece
+                if (pieceOnSquare.getTeamColor() != board.getPiece(startPos).getTeamColor()) {
+                    validMoves.add(new ChessMove(startPos, possiblePos, null));
+                }
+                break;
+            }
+        }
+        col = startPos.getColumn();
+        // then check right
+        while (col < 8) {
+            col += 1;
+            var possiblePos = new ChessPosition(row, col);
+            ChessPiece pieceOnSquare = board.getPiece(possiblePos);
+            if (pieceOnSquare == null) {
+                validMoves.add(new ChessMove(startPos, possiblePos, null));
+            } else {
+                // Only add possible move if the piece on square is an enemy piece
+                if (pieceOnSquare.getTeamColor() != board.getPiece(startPos).getTeamColor()) {
+                    validMoves.add(new ChessMove(startPos, possiblePos, null));
+                }
+                break;
+            }
+        }
     }
 
     public static void getDiagonalMoves(ArrayList<ChessMove> validMoves, ChessBoard board, ChessPosition startPos) {
