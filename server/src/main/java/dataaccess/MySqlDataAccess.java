c6cf9b73d77778b9749e165f8c887a19e61b37af
Helaman Bennion <helama22@byu.edu>
2024-10-28 21:58:39 -0600

    setup first query to database (unfinished)

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -1,5 +1,8 @@
 package dataaccess;
 
+import model.UserData;
+import server.ResponseException;
+
 import java.sql.Connection;
 
 public class MySqlDataAccess implements DataAccess {
@@ -7,6 +10,34 @@ public class MySqlDataAccess implements DataAccess {
 
     public MySqlDataAccess() {
         this.configure();
+    }
+
+    public static class UserDAO extends DataAccess.UserDAO {
+
+        public UserData getUser(UserData userData) {
+            try (var conn = DatabaseManager.getConnection()) {
+                var statement = "SELECT username, json FROM user WHERE username=?";
+                try (var ps = conn.prepareStatement(statement)) {
+                    ps.setString(1, userData.username());
+                    try (var rs = ps.executeQuery()) {
+                        if (rs.next()) {
+                            return readUser(rs);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+               // throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+            }
+            return null;
+        }
+
+        public void createUser(UserData userData) {
+
+        }
+
+        public void clearAllUsers() {
+
+        }
     }
 
     private void configure() {
