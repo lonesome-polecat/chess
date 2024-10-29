bef267749060d569509f0e95f7efffe0d5919a3e
Helaman Bennion <helama22@byu.edu>
2024-10-29 09:20:18 -0600

    add create statements

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -1,22 +1,19 @@
 package dataaccess;
 
-import chess.ChessGame;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
 import server.ResponseException;
 
 import java.sql.Connection;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.sql.SQLException;
 import java.util.List;
-import java.util.UUID;
 
 public class MySqlDataAccess implements DataAccess {
     Connection database;
 
     public MySqlDataAccess() {
-        this.configure();
+        this.configureDatabase();
     }
 
 
@@ -84,23 +81,50 @@ public class MySqlDataAccess implements DataAccess {
 
     private final String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS  user (
+            CREATE TABLE IF NOT EXISTS user (
               `username` varchar(256) NOT NULL,
               `password` varchar(256) NOT NULL,
               `email` varchar(256) NOT NULL,
-              `json` TEXT DEFAULT NULL,
               PRIMARY KEY (`username`),
               INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+              `username` varchar(256) NOT NULL,
+              `authToken` varchar(256) NOT NULL,
+              PRIMARY KEY (`authToken`),
+              INDEX(authToken)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS game (
+              `gameID` int NOT NULL AUTO_INCREMENT,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`gameID`),
+              INDEX(gameID)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
 
-    private void configure() {
+    private void configureDatabase() throws ResponseException {
         try {
             DatabaseManager.createDatabase();
-            database = DatabaseManager.getConnection();
-        } catch (DataAccessException e) {
-            System.out.println(e);
+        } catch (DataAccessException ex) {
+            throw new ResponseException(500, String.format("Unable to create database: %s", ex.getMessage()));
+        }
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (DataAccessException ex) {
+            throw new ResponseException(500, String.format("Unable to connect to database: %s", ex.getMessage()));
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
         }
     }
 }
