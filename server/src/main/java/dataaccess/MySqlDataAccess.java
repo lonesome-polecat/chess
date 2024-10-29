d90afc55defc1172a8b6c8fd346fe633328a6d4a
Helaman Bennion <helama22@byu.edu>
2024-10-29 08:43:24 -0600

    add all database methods (empty)

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -82,6 +82,19 @@ public class MySqlDataAccess implements DataAccess {
         }
     }
 
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  user (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
     private void configure() {
         try {
             DatabaseManager.createDatabase();
