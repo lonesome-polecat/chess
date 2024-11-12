24efaa638d53449ecb4f2586e15af2441ce20e64
Helaman Bennion <helama22@byu.edu>
2024-11-12 11:39:10 -0700

    enable login and logout methods in repl

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -4,7 +4,6 @@ import model.UserData;
 import server.ResponseException;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class ChessClient {
     private final ServerFacade server;
@@ -28,13 +27,13 @@ public class ChessClient {
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
-//                case "signin" -> signIn(params);
+                case "signin" -> signIn(params);
                 case "register" -> register(params);
 //                case "list" -> listPets();
-//                case "signout" -> signOut();
+                case "signout" -> signOut();
 //                case "adopt" -> adoptPet(params);
 //                case "adoptall" -> adoptAllPets();
-                case "quit" -> quit();
+                case "quit" -> "quit";
                 default -> help();
             };
         } catch (ResponseException ex) {
@@ -74,10 +73,30 @@ public class ChessClient {
         }
         server.registerUser(new UserData(username, password, email));
         state = State.SIGNED_IN;
-        return String.format("Successfully registered new user %s", username);
+        return String.format("Successfully registered new user\nYou are now signed in as %s", username);
     }
 
-    public String quit() throws ResponseException {
-        throw new ResponseException(500, "Not implemented");
+    public String signIn(String[] params) throws ResponseException {
+        for (var param : params) {
+            System.out.println(param);
+        }
+        if (params.length != 2) {
+            throw new ResponseException(400, "You must enter a username and password to sign in");
+        }
+        var username = params[0];
+        var password = params[1];
+        server.loginUser(new UserData(username, password, null));
+        state = State.SIGNED_IN;
+        return String.format("You are signed in as %s", username);
+    }
+
+    public String signOut() throws ResponseException {
+        try {
+            server.logoutUser();
+        } catch (ResponseException e) {
+            throw new ResponseException(401, "Error: unable to sign out user");
+        }
+        state = State.SIGNED_OUT;
+        return "You are successfully signed out";
     }
 }
